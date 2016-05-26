86f18beab619285d761481af5011ecbe9ca9fc8b
Brian Silvia <brian@brinkbit.com>
2016-05-25 21:53:38 -0400

    Refactor to accept external mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const logger = require( 'brinkbit-logger' )({ __filename, transport: 'debug' });
+const logger = require( 'brinkbit-logger' )({ __filename, transport: 'production' });
 
 const port = process.env.PORT || 3000;
 const mongodb = {
@@ -12,18 +12,10 @@ const mongodb = {
 mongodb.uri = mongodb.user && mongodb.pass ?
     `mongodb://${mongodb.user}:${mongodb.pass}@${mongodb.ip}:${mongodb.port}/test` :
     `mongodb://${mongodb.ip}:${mongodb.port}/test`;
-const cryptoKey = process.env.CRYPTO_KEY;
-const loginAttempts = {
-    forIp: 50,
-    forIpAndUser: 7,
-    logExpiration: '20m',
-};
 
-function connect( mongoose ) {
-    const conn = mongoose.connection;
+function connect( mongooseConnection ) {
     return new Promise(( resolve, reject ) => {
-        console.log( 'connection is undefined here' );
-        if ( conn.readyState === 1 ) {
+        if ( mongooseConnection.readyState === 1 ) {
             logger.warning( 'Already connected. Resolving.' );
             return resolve();
         }
@@ -31,37 +23,39 @@ function connect( mongoose ) {
         const mongodbURI = mongodb.uri;
 
         logger.info( `Attempting to connect to: ${mongodbURI}` );
-        mongoose.connect( mongodbURI );
-        conn.on( 'error', ( err ) => reject( err ));
-        conn.on( 'connected', () => {
+        mongooseConnection
+        .removeAllListeners( 'connected' ).on( 'connected', () => {
             logger.info( 'Connected!' );
             resolve();
-        });
+        })
+        .removeAllListeners( 'error' ).on( 'error', ( err ) => reject( err ))
+        .open( mongodbURI );
     });
 }
 
-function disconnect( mongoose ) {
-    const conn = mongoose.connection;
-    return new Promise(( resolve ) => {
-        if ( conn.readyState === 0 ) {
+function disconnect( mongooseConnection ) {
+    return new Promise(( resolve, reject ) => {
+        if ( mongooseConnection.readyState === 0 ) {
             logger.warning( 'No connection to disconnect. Resolving.' );
             return resolve();
         }
 
         logger.info( 'Closing mongoose connection' );
-        conn.close();
+        mongooseConnection
+        .removeAllListeners( 'close' ).on( 'close', () => {
+            logger.info( 'Mongoose connection closed' );
+            resolve();
+        })
+        .removeAllListeners( 'error' ).on( 'error', ( err ) => reject( err ))
+        .close();
     });
 }
 
-module.exports = ( mongoose ) => {
-    const conn = mongoose.connection;
-    mongoose.Promise = Promise;
+module.exports = ( mongooseConnection ) => {
     return {
         port,
         mongodb,
-        cryptoKey,
-        loginAttempts,
-        connect: () => connect( conn ),
-        disconnect: () => disconnect( conn ),
+        connect: () => connect( mongooseConnection ),
+        disconnect: () => disconnect( mongooseConnection ),
     };
 };
--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const chai = require( 'chai' );
 const chaiaspromised = require( 'chai-as-promised' );
 const expect = chai.expect;
 const mongoose = require( 'mongoose' );
-const db = require( '../index.js' )( mongoose );
+const db = require( '../index.js' )( mongoose.connection );
 
 chai.use( chaiaspromised );
 
@@ -13,27 +13,24 @@ it( 'should have the appropriate default configuration for testing', () => {
     expect( db.mongodb ).to.have.property( 'ip', 'localhost' );
     expect( db.mongodb ).to.have.property( 'port', '27017' );
     expect( db.mongodb ).to.have.property( 'uri', 'mongodb://localhost:27017/test' );
-    expect( db.loginAttempts ).to.have.property( 'forIp', 50 );
-    expect( db.loginAttempts ).to.have.property( 'forIpAndUser', 7 );
-    expect( db.loginAttempts ).to.have.property( 'logExpiration', '20m' );
 });
 
 it( 'should resolve when it tries to disconnect without a connection', () => {
-    expect( db.disconnect( )).to.eventually.resolve;
+    return expect( db.disconnect( )).to.eventually.be.fulfilled;
 });
 
 it( 'should be able to connect with the default configuration', () => {
-    expect( db.connect( )).to.eventually.resolve;
+    return expect( db.connect( )).to.eventually.be.fulfilled;
 });
 
 it( 'should be able to disconnect after connection', () => {
-    expect( db.disconnect( )).to.eventually.resolve;
+    return expect( db.disconnect( )).to.eventually.be.fulfilled;
 });
 
 it( 'should resolve if it is already connected', ( done ) => {
-    db.connect()
+    return db.connect()
     .then(() => {
-        expect( db.connect( )).to.eventually.resolve;
+        expect( db.connect( )).to.eventually.be.fulfilled;
     })
     .then( done );
 });
