2c0a140011098bd9c28786959f01cc931064b962
Justin Livi <justin@brinkbit.com>
2016-07-11 17:21:23 -0400

    Add code coverage and error logging

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongodb = {
     port: process.env.MONGO_PORT || '27017',
     user: process.env.MONGO_USER,
     pass: process.env.MONGO_PASS,
-    database: process.env.MONGO_DB || 'test', 
+    database: process.env.MONGO_DB || 'test',
 };
 mongodb.uri = mongodb.user && mongodb.pass ?
     `mongodb://${mongodb.user}:${mongodb.pass}@${mongodb.ip}:${mongodb.port}/${mongodb.database}` :
@@ -21,16 +21,20 @@ function connect( mongooseConnection ) {
             return resolve();
         }
 
-        const mongodbURI = mongodb.uri;
-
-        logger.info( `Attempting to connect to: ${mongodbURI}` );
+        logger.info( `Attempting to connect to: ${mongodb.uri}` );
         mongooseConnection
         .removeAllListeners( 'connected' ).on( 'connected', () => {
             logger.info( 'Connected!' );
             resolve();
         })
-        .removeAllListeners( 'error' ).on( 'error', ( err ) => reject( err ))
-        .open( mongodbURI );
+        .removeAllListeners( 'error' ).on( 'error', ( err ) => {
+            logger.crit( `mongoose connection error: "${err.message}"`, {
+                message: err.message,
+                uri: mongodb.uri,
+            });
+            reject( err );
+        })
+        .open( mongodb.uri );
     });
 }
 
@@ -47,7 +51,13 @@ function disconnect( mongooseConnection ) {
             logger.info( 'Mongoose connection closed' );
             resolve();
         })
-        .removeAllListeners( 'error' ).on( 'error', ( err ) => reject( err ))
+        .removeAllListeners( 'error' ).on( 'error', ( err ) => {
+            logger.crit( `mongoose connection error: "${err.message}"`, {
+                message: err.message,
+                uri: mongodb.uri,
+            });
+            reject( err );
+        })
         .close();
     });
 }
