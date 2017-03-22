9eb8dc0e7f8c787eed26590d3ee147cfa9afa40a
Justin Livi <justin@brinkbit.com>
2017-03-22 17:18:50 -0400

    Support multiple connections, sharding and replica sets

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-'use strict';
-
+const mongoose = require( 'mongoose' );
 const logger = require( 'brinkbit-logger' )({ __filename });
 
-function connect( mongodb, mongooseConnection ) {
+function getConnection( mongodb, connection ) {
+    if ( !connection ) {
+        return mongoose.connection;
+    }
+    if ( typeof connection.createConnection === 'function' ) {
+        return connection.createConnection( mongodb.uri, { });
+    }
+    if (
+        typeof connection.removeAllListeners === 'function' &&
+        typeof connection.open === 'function'
+    ) {
+        return connection;
+    }
+    return null;
+}
+
+function connect( mongodb, connection ) {
     return new Promise(( resolve, reject ) => {
+        const mongooseConnection = getConnection( mongodb, connection );
+        if ( !mongooseConnection ) {
+            logger.crit( 'mongoose connection error: "Invalid connection"', {
+                message: 'Invalid connection',
+                uri: mongodb.uri,
+            });
+            reject( new Error( 'Invalid connection' ));
+        }
+
         if ( mongooseConnection.readyState === 1 ) {
             logger.warning( 'Already connected. Resolving.' );
-            return resolve();
+            return resolve( mongooseConnection );
         }
 
         logger.info( `Attempting to connect to: ${mongodb.uri}` );
         mongooseConnection
         .removeAllListeners( 'connected' ).on( 'connected', () => {
             logger.info( 'Connected!' );
-            resolve();
+            resolve( mongooseConnection );
         })
         .removeAllListeners( 'error' ).on( 'error', ( err ) => {
             logger.crit( `mongoose connection error: "${err.message}"`, {
@@ -21,8 +45,12 @@ function connect( mongodb, mongooseConnection ) {
                 uri: mongodb.uri,
             });
             reject( err );
-        })
-        .open( mongodb.uri );
+        });
+
+        if ( mongooseConnection.readyState === 0 ) {
+            return mongooseConnection.open( mongodb.uri );
+        }
+        return logger.warning( 'Already connecting' );
     });
 }
 
@@ -34,7 +62,7 @@ function disconnect( mongodb, mongooseConnection ) {
         }
 
         logger.info( 'Closing mongoose connection' );
-        mongooseConnection
+        return mongooseConnection
         .removeAllListeners( 'close' ).on( 'close', () => {
             logger.info( 'Mongoose connection closed' );
             resolve();
@@ -50,21 +78,54 @@ function disconnect( mongodb, mongooseConnection ) {
     });
 }
 
+function createMongosConfig( mongos ) {
+    const upstreamConfigs = mongos || [];
+    if ( !upstreamConfigs.length && !process.env.MONGO_MONGOS_COUNT ) return undefined;
+    const configs = [];
+    let count = 0;
+    if ( upstreamConfigs.length > 0 ) {
+        count = upstreamConfigs.length;
+    }
+    else if ( process.env.MONGO_MONGOS_COUNT ) {
+        count = process.env.MONGO_MONGOS_COUNT;
+    }
+    for ( let i = 0; i < count; i += 1 ) {
+        const config = upstreamConfigs[i] || {};
+        const newConfig = {
+            host: config.host || process.env[`MONGO_HOST_${i}`] || `${process.env.MONGO_HOST || 'db'}-${i + 1}`,
+            port: config.port || process.env[`MONGO_PORT_${i}`] || process.env.MONGO_PORT || '27017',
+        };
+        newConfig.string = `${newConfig.host}:${newConfig.port}`;
+        configs.push( newConfig );
+    }
+    return configs.length ? configs : undefined;
+}
+
 module.exports = ( mongooseConnection, config ) => {
     const upstreamConfig = config || {};
     const mongodb = {
-        ip: upstreamConfig.ip || process.env.MONGO_IP || 'localhost',
+        host: upstreamConfig.host || process.env.MONGO_HOST || upstreamConfig.ip || process.env.MONGO_IP || 'localhost',
         port: upstreamConfig.port || process.env.MONGO_PORT || '27017',
         user: upstreamConfig.user || process.env.MONGO_USER,
         pass: upstreamConfig.pass || process.env.MONGO_PASS,
         database: upstreamConfig.database || process.env.MONGO_DB || 'test',
+        mongos: createMongosConfig( upstreamConfig.mongos ),
+        options: upstreamConfig.options,
+        replicaSet: upstreamConfig.replicaSet || process.env.MONGO_REPLICA_SET,
     };
-    mongodb.uri = mongodb.user && mongodb.pass ?
-        `mongodb://${mongodb.user}:${mongodb.pass}@${mongodb.ip}:${mongodb.port}/${mongodb.database}` :
-        `mongodb://${mongodb.ip}:${mongodb.port}/${mongodb.database}`;
+    const hostsAndPorts = mongodb.mongos ?
+        mongodb.mongos.map( mongo => `${mongo.host}:${mongo.port}` ).join( ',' ) :
+        `${mongodb.host}:${mongodb.port}`;
+    const replicaString = mongodb.replicaSet ? `?replicaSet=${mongodb.replicaSet}` : '';
+    mongodb.uri = upstreamConfig.uri || process.env.MONGO_URI || ( mongodb.user && mongodb.pass ?
+        `mongodb://${mongodb.user}:${mongodb.pass}@${hostsAndPorts}/${mongodb.database}${replicaString}` :
+        `mongodb://${hostsAndPorts}/${mongodb.database}${replicaString}` );
     return {
         config: mongodb,
         connect: () => connect( mongodb, mongooseConnection ),
-        disconnect: () => disconnect( mongodb, mongooseConnection ),
+        disconnect: connection => disconnect(
+            mongodb,
+            connection || mongooseConnection || mongoose.connection
+        ),
     };
 };
--- test/index.js
+++ test/index.js
@@ -1,10 +1,8 @@
-'use strict';
-
 const chai = require( 'chai' );
 const chaiaspromised = require( 'chai-as-promised' );
-const expect = chai.expect;
 const mongoose = require( 'mongoose' );
 
+const expect = chai.expect;
 chai.use( chaiaspromised );
 
 describe( 'mongoose configuration', function() {
@@ -17,7 +15,10 @@ describe( 'mongoose configuration', function() {
 
         it( 'should have the appropriate default configuration', function() {
             expect( db.config ).to.deep.equal({
-                ip: 'localhost',
+                host: 'localhost',
+                mongos: undefined,
+                options: undefined,
+                replicaSet: undefined,
                 pass: undefined,
                 user: undefined,
                 port: '27017',
@@ -48,58 +49,161 @@ describe( 'mongoose configuration', function() {
 
     describe( 'environment variable configuration', function() {
         it( 'should have the appropriate configuration', function() {
-            process.env.MONGO_IP = 'testIp';
+            process.env.MONGO_HOST = 'testHost';
             process.env.MONGO_PORT = 'testPort';
             process.env.MONGO_USER = 'testUser';
             process.env.MONGO_PASS = 'testPassword';
             process.env.MONGO_DB = 'testDB';
             const db = require( '../src' )( mongoose.connection );
             expect( db.config ).to.deep.equal({
-                ip: 'testIp',
+                host: 'testHost',
+                mongos: undefined,
+                options: undefined,
+                replicaSet: undefined,
                 port: 'testPort',
                 user: 'testUser',
                 pass: 'testPassword',
-                uri: 'mongodb://testUser:testPassword@testIp:testPort/testDB',
+                uri: 'mongodb://testUser:testPassword@testHost:testPort/testDB',
                 database: 'testDB',
             });
+        });
+
+        describe( 'MONGO_MONGOS_COUNT simple configuration', function() {
+            it( 'should have the appropriate configuration', function() {
+                process.env.MONGO_MONGOS_COUNT = 2;
+                process.env.MONGO_HOST = 'testHost';
+                process.env.MONGO_PORT = 'testPort';
+                process.env.MONGO_USER = 'testUser';
+                process.env.MONGO_PASS = 'testPassword';
+                process.env.MONGO_DB = 'testDB';
+                const db = require( '../src' )( mongoose.connection );
+                expect( db.config ).to.deep.equal({
+                    host: 'testHost',
+                    mongos: [{
+                        port: 'testPort',
+                        host: 'testHost-1',
+                        string: 'testHost-1:testPort',
+                    }, {
+                        port: 'testPort',
+                        host: 'testHost-2',
+                        string: 'testHost-2:testPort',
+                    }],
+                    options: undefined,
+                    replicaSet: undefined,
+                    port: 'testPort',
+                    user: 'testUser',
+                    pass: 'testPassword',
+                    uri: 'mongodb://testUser:testPassword@testHost-1:testPort,testHost-2:testPort/testDB',
+                    database: 'testDB',
+                });
+            });
+        });
+
+        afterEach( function() {
+            delete process.env.MONGO_MONGOS_COUNT;
+            delete process.env.MONGO_HOST;
+            delete process.env.MONGO_PORT;
+            delete process.env.MONGO_USER;
+            delete process.env.MONGO_PASS;
+            delete process.env.MONGO_DB;
         });
     });
 
     describe( 'manual configuration', function() {
         it( 'should have the appropriate configuration', function() {
-            process.env.MONGO_IP = 'testIp';
+            process.env.MONGO_HOST = 'testHost';
             process.env.MONGO_PORT = 'testPort';
             process.env.MONGO_USER = 'testUser';
             process.env.MONGO_PASS = 'testPassword';
             process.env.MONGO_DB = 'testDB';
             const db = require( '../src' )( mongoose.connection, {
-                ip: 'manualIp',
+                host: 'manualHost',
                 port: 'manualPort',
+                database: 'manualDB',
                 user: 'manualUser',
                 pass: 'manualPassword',
-                database: 'manualDB',
+                replicaSet: 'manualReplica',
+                mongos: [{
+                    host: 'host1',
+                    port: 'port1',
+                }],
+                ip: 'manualIp',
+                options: {},
             });
             expect( db.config ).to.deep.equal({
-                ip: 'manualIp',
+                host: 'manualHost',
+                mongos: [{
+                    host: 'host1',
+                    port: 'port1',
+                    string: 'host1:port1',
+                }],
+                options: {},
+                replicaSet: 'manualReplica',
                 port: 'manualPort',
                 user: 'manualUser',
                 pass: 'manualPassword',
-                uri: 'mongodb://manualUser:manualPassword@manualIp:manualPort/manualDB',
+                uri: 'mongodb://manualUser:manualPassword@host1:port1/manualDB?replicaSet=manualReplica',
                 database: 'manualDB',
             });
+        });
+
+        after( function() {
+            delete process.env.MONGO_HOST;
+            delete process.env.MONGO_PORT;
+            delete process.env.MONGO_USER;
+            delete process.env.MONGO_PASS;
+            delete process.env.MONGO_DB;
+        });
+    });
+
+    describe( 'no connection provided', function() {
+        let db;
+        it( 'should be able to connect with the default configuration', function() {
+            db = require( '../src' )();
+            return expect( db.connect()).to.eventually.be.fulfilled;
+        });
+
+        after( function() {
+            return db.disconnect();
+        });
+    });
+
+    describe( 'mongoose instance provided', function() {
+        const db = require( '../src' )( mongoose );
+        let connection;
+        it( 'should create and resolve with a new connection', function() {
+            return db.connect()
+            .then(( res ) => {
+                connection = res;
+                expect( connection ).to.not.equal( mongoose.connection );
+            });
+        });
+
+        after( function() {
+            return db.disconnect( connection );
         });
     });
 
     describe( 'edge cases', function() {
-        const db = require( '../src' )( mongoose.connection, {
-            ip: 'invalid',
-            port: 'invalid',
-            user: 'invalid',
-            pass: 'invalid',
-            database: 'invalid',
+        it( 'should reject on connect error', function() {
+            const db = require( '../src' )( mongoose.connection, {
+                host: 'invalid',
+                port: 'invalid',
+                user: 'invalid',
+                pass: 'invalid',
+                database: 'invalid',
+            });
+            return expect( db.connect()).to.be.rejected;
         });
 
-        it( 'should reject on connect error', function() {
+        it( 'should reject on invalid connection', function() {
+            const db = require( '../src' )({}, {
+                host: 'invalid',
+                port: 'invalid',
+                user: 'invalid',
+                pass: 'invalid',
+                database: 'invalid',
+            });
             return expect( db.connect()).to.be.rejected;
         });
     });
